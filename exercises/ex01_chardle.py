af5e4a809363d2afa009925f6f7129c983282067
Abby Guerra <aeguerra@unc.edu>
2023-01-24 13:39:27 -0500

    final

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -17,23 +17,23 @@ else:
 
         if (character == word [0]): 
             print (character + " found at " + "index 0")
-            count+=1
+            count += 1
         if (character == word [1]):
             print (character + " found at " + "index 1")
-            count+=1
+            count += 1
         if (character == word [2]):
             print (character + " found at " + "index 2")
-            count+=1
+            count += 1
         if (character == word [3]):
             print (character + " found at " + "index 3")
             count+=1
         if (character == word [4]):
             print (character + " found at " + "index 4")
-            count+=1
+            count += 1
 
-        if count ==0: 
+        if count == 0: 
             print ("No instances of " + character + " found in " + word)
-        if count ==1:
+        if count == 1:
             print ("1 instance of " + character + " found in " + word)
         else: 
             print (str(count) + " instances of " + character + " found in " + word)
