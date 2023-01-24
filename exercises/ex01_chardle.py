175ad0b97333c0879c9b05e3791e10d88641c0f3
Abby Guerra <aeguerra@unc.edu>
2023-01-24 13:20:26 -0500

    EX01 changes

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,4 +1,4 @@
-"""EX01 - Chardle - A cute step toward Wordle"""
+"""EX01 - Chardle - A cute step toward Wordle."""
 
 __author__ = "730556346"
 
@@ -6,34 +6,32 @@ word: str = input ("Enter a 5-character word: ")
 if len(word) != 5: 
     print ("Error: Word must contain 5 characters")
 else:
-    character: str = input("Enter a single character: ")
-    print("Searching for " + character + " in " + word)
-    counter: int = 0
+    character: str = input ("Enter a single character: ")
+    if len(character) != 1:
+        print ("Error: Character must be a single character.")
+    else:
+        print("Searching for " + character + " in " + word)
+        count: int = 0
 
-    if (character == word [0]): 
-        print (character + " found at " + "index 0")
-        counter+=1
-    if (character == word [1]):
-        print (character + " found at " + "index 1")
-        counter+=1
-    if (character == word [2]):
-        print (character + " found at " + "index 2")
-        counter+=1
-    if (character == word [3]):
-        print (character + " found at " + "index 3")
-        counter+=1
-    if (character == word [4]):
-        print (character + " found at " + "index 4")
-        counter+=1
+        if (character == word [0]): 
+            print (character + " found at " + "index 0")
+            count+=1
+        if (character == word [1]):
+            print (character + " found at " + "index 1")
+            count+=1
+        if (character == word [2]):
+            print (character + " found at " + "index 2")
+            count+=1
+        if (character == word [3]):
+            print (character + " found at " + "index 3")
+            count+=1
+        if (character == word [4]):
+            print (character + " found at " + "index 4")
+            count+=1
 
-    if counter ==0: 
-        print ("No instances of " + character + " found in " + word)
-    else: 
-        print(str(counter) + " instances of " + character + " found in " + word)
-
-
-
-
-
-
-
+        if count ==0: 
+            print ("No instances of " + character + " found in " + word)
+        if count ==1:
+            print ("1 instance of " + character + " found in " + word)
+        else: 
+            print(str(count) + " instances of " + character + " found in " + word)
