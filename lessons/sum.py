08328d47b9c686a5d241d6ba1b1b5348fac4f521
Abby Guerra <aeguerra@unc.edu>
2023-03-05 13:28:22 -0500

    new

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,9 +1,8 @@
 """Example function for unit tests."""
 
-def sum(xs:"list[float]") -> float: 
-    """return sum of all elements in xs"""
+def sum (xs: 'list[float]') -> float: 
+    """return sum of all elements in xs."""
     sum: int = 0
-    for elem in xs: 
-        sum = sum + elem 
-    return sum
-    
+    for elem in range(0,len(xs)):
+        sum = sum + xs[elem]
+    return sum 
