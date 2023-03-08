2120fb13337ecb691cf2105131883e027e9a6317
Abby Guerra <aeguerra@unc.edu>
2023-03-08 15:50:26 -0500

    nwq

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,24 @@
+"""List utility functions."""
+
+__author__ = "730556346"
+
+def only_evens(input: int) -> int: 
+    """Listing only even numbers."""
+    even_list = ""
+    for num in input: 
+        if num % 2 == 0:
+            even_list.append(num)
+    return even_list 
+
+def concat(list1: int, list2: int) -> int: 
+    """"""
+    new_list = ""
+    for num in list1: 
+        new_list.append(num)
+    for num in list2: 
+        new_list.append(num)
+    return new_list
+
+def sub(list:'list[int]',start: int, end: int) -> int:
+    """"""
+    return list[start:end]
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,35 @@
+"""Unit test functions."""
+
+__author__ = "730556346"
+
+from exercises.ex05.utils import only_evens, sub, concat
+
+def test_only_evens(): 
+    """Edge case."""
+    assert only_evens[()] == []
+
+    """Use case 1."""
+    assert only_evens[(1,2,3,4,5,6)] == [2,4,6]
+
+    """Use case 2."""
+    assert only_evens[(1,3,5,7,9,11)] == []
+
+def test_concat():
+    """Edge case."""
+    assert concat[(),()] == []
+    
+    """Use case 1."""
+    assert concat[(1,2,3),(4,5,6)] == [1,2,3,4,5,6]
+
+    """Use case 2."""
+    assert concat[(1,2,3),()] == [1,2,3]
+
+def test_sub():
+    """Edge case."""
+    assert sub[(),0,0] == []
+    
+    """Use case 1."""
+    assert sub[(1,2,3),-1,2,3] == [1,2]
+
+    """Use case 2."""
+    assert sub[(1,2,3,4,5,6),1,4,6] == [2,3,4,6]
