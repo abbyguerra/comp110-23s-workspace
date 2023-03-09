72dc0b5908e95a725f9f881f5e2f57414b3320f3
Abby Guerra <aeguerra@unc.edu>
2023-03-08 21:47:04 -0500

    new

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,23 +2,29 @@
 
 __author__ = "730556346"
 
-def only_evens(input: 'list[int]') -> list(int): 
+def only_evens(input: 'list(int)') -> int:
     """Listing only even numbers."""
-    even_list = ""
+    even_list = []
     for num in input: 
         if num % 2 == 0:
             even_list.append(num)
     return even_list 
 
-def concat(list1: 'list[int]', list2: 'list[int]') -> list(int): 
-    """"""
-    new_list = ""
+def concat(list1: 'list(int)', list2: 'list(int)') -> int:
+    """Combining two lists"""
+    new_list = []
     for num in list1: 
         new_list.append(num)
     for num in list2: 
         new_list.append(num)
     return new_list
 
-def sub(list:'list[int]',start: int, end: int) -> list(int):
-    """"""
+def sub(list:'list(int)',start: int, end: int) -> 'list(int)':
+    """Making a subset of a list"""
+    if len(list) == 0 or start >= len(list) or end <= 0: 
+        return []
+    if start < 0:
+        start = 0 
+    if end > len(list): 
+        end = len(list)
     return list[start:end]
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -5,31 +5,25 @@ __author__ = "730556346"
 from exercises.ex05.utils import only_evens, sub, concat
 
 def test_only_evens() -> None:
-    """Edge case."""
-    assert only_evens[()] == []
-
-    """Use case 1."""
-    assert only_evens[(1,2,3,4,5,6)] == [2,4,6]
-
-    """Use case 2."""
-    assert only_evens[(1,3,5,7,9,11)] == []
+    """Edge case - testing with an empty list"""
+    assert only_evens([]) == []
+    """Use case 1 - testing even and odd numbers"""
+    assert only_evens([1,2,3,4,5,6]) == [2,4,6]
+    """Use case 2 - testing with only odd numbers"""
+    assert only_evens([1,3,5,7,9,11]) == []
 
 def test_concat() -> None:
-    """Edge case."""
-    assert concat[(),()] == []
-    
-    """Use case 1."""
-    assert concat[(1,2,3),(4,5,6)] == [1,2,3,4,5,6]
-
-    """Use case 2."""
-    assert concat[(1,2,3),()] == [1,2,3]
+    """Edge case - testing with empty lists."""
+    assert concat ([],[]) == []
+    """Use case 1 - testing even and odd numbers"""
+    assert concat ([1,2,3],[4,5,6]) == [1,2,3,4,5,6]
+    """Use case 2 - testing with one empty list"""
+    assert concat([1,2,3],[]) == [1,2,3]
 
 def test_sub() -> None: 
-    """Edge case."""
-    assert sub[(),0,0] == []
-    
-    """Use case 1."""
-    assert sub[(1,2,3),-1,2,3] == [1,2]
-
-    """Use case 2."""
-    assert sub[(1,2,3,4,5,6),1,4,6] == [2,3,4,6]
+    """Edge case - testing with an empty list"""
+    assert sub([],0,0) == []
+    """Use case 1 - testing with negative numbers"""
+    assert sub([1,2,3],-1,2) == [1,2]
+    """Use case 2 - testing a range of indices in the list """
+    assert sub([1,2,3,4,5,6],1,5) == [2,3,4,5]
