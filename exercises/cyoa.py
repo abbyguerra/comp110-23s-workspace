4c5b522e4ee526ca6fa0cc0163daaafa40eb88f2
Abby Guerra <aeguerra@unc.edu>
2023-03-22 14:39:40 -0400

    new

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -15,11 +15,6 @@ EMOJI_HEADS = "\U0001f1ED"
 EMOJI_TAILS = "\U0001F1F9"
 
 
-def flip() -> None:
-    """Simulates a coin flip."""
-    return random.choice(['heads', 'tails'])
-
-
 def main() -> None:
     """The function that runs the guessing game."""
     greet()
@@ -30,7 +25,7 @@ def main() -> None:
 def greet() -> None: 
     """Greeting the player and asking for their name."""
     global player
-    player = input("Before we begin, what is your name? ")
+    player: str = input("Before we begin, what is your name? ")
     print(f"Welcome {player}! In this game you will be asked to guess the correct side of coin before it is flipped entering either 'h' or 't'.\n ")
     return None
     
@@ -38,11 +33,11 @@ def greet() -> None:
 # Function for coin flip 
 def coin_flip(points: int) -> int:
     """Asks the player for their guess, flips a coin, and updates their score accordingly."""
-    guess = input(f"\n{player}, please guess if the coin will land on heads ({EMOJI_HEADS}) or tails ({EMOJI_TAILS})")
+    guess: str = input(f"\n{player}, please guess if the coin will land on heads ({EMOJI_HEADS}) or tails ({EMOJI_TAILS})")
     if guess != "heads" and guess != "tails": 
         print("Invalid input. Please enter 'heads' or 'tails. ")
         return coin_flip(points)
-    result = random.choice(["heads", "tails"])
+    result: str = random.choice(["heads", "tails"])
     print(f"The coin landed on {result} {EMOJI_COIN}")
     if result == guess: 
         points += 1
@@ -59,7 +54,7 @@ def more(correct_number: int) -> None:
     global points
     print(f"\n{player}, let's try something different.\n")
     print("Please guess a number between 1 and 10. You will have 3 guesses and each incorrect guess will deduct 1 point from your total score.")
-    correct_number = random.randint(1, 10)
+    correct_number: int = random.randint(1, 10)
     for i in range(3): 
         guess = int(input(f"Attempt {i+1}: "))
         if guess == correct_number: 
@@ -78,7 +73,7 @@ def loop(choice: int) -> None:
     global points
     while True: 
         print("What would you like to do next?\n1. Guess the side of the coin\n2. Additional interaction\n3. Quit the game\n ")
-        choice = input("Enter the number of your choice: ")
+        choice: int = input("Enter the number of your choice: ")
         if choice == "1": 
             points = coin_flip(points)
         if choice == "2": 
